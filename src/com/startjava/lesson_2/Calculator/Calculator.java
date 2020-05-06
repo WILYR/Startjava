c813ac2bc2464675d7160cf2ecbaeb097ab2b135
Nikita\WILYR <wilyrjvdv@gmail.com>
2020-05-06 19:24:00 +0300

    Rework calculator with arrays

--- src/com/startjava/lesson_2/Calculator/Calculator.java
+++ src/com/startjava/lesson_2/Calculator/Calculator.java
@@ -1,67 +1,51 @@
 package com.startjava.lesson_2.Calculator;
 
 public class Calculator {
-	private float num1;
-	private float num2;
-	private String operation;
+    private String str;
+    private char operation = '0';
+    private String[] arrOf;
 
-	public float getNum1() {
-		return num1;
-	}
+    public void takeSymbols(String str) {
+        for (int j = 0; j < str.length(); j++) {
+            operation = str.charAt(j);
+            if(operation == '+' || operation == '-' || operation == '/' || operation == '*' || operation == '^' || operation == '%') {
+                break;
+            } else {
+                operation = '0';
+            }
+        }
+        arrOf = str.split("[+\\-*/^%]");
+    }
 
-	public void setNum1(float num1) {
-		this.num1 = num1;
-	}
-
-	public float getNum2() {
-		return num2;
-	}
-
-	public void setNum2(float num2) {
-		this.num2 = num2;
-	}
-
-	public String getOperation() {
-		return operation;
-	}
-
-	public void setOperation(String operation) {
-		this.operation = operation;
-	}
-
-	public float calculate() {
-		float result = 0;
+	public void calculate() {
+		float result;
 		switch(operation) {
-			case "+": 
-				result = num1 + num2;
+			case '+':
+				result = Float.parseFloat(arrOf[0]) + Float.parseFloat(arrOf[1]);
+                System.out.println("Ответ: " + result);
 				break;
-			case "-": 
-				result = num1 - num2;
+			case '-':
+                result = Float.parseFloat(arrOf[0]) - Float.parseFloat(arrOf[1]);
+                System.out.println("Ответ: " + result);
 				break;
-			case "*": 
-				result = num1 * num2;
+			case '*':
+                result = Float.parseFloat(arrOf[0]) * Float.parseFloat(arrOf[1]);
+                System.out.println("Ответ: " + result);
 				break;
-			case "%": 
-				result = num1 % num2;
+			case '%':
+                result = Float.parseFloat(arrOf[0]) % Float.parseFloat(arrOf[1]);
+                System.out.println("Ответ: " + result);
 				break;
-			case "^": 
-				result = num1;
-				if (num2 > 0) {
-					for(int i = 0; i < (num2 - 1); i++) {
-						result *= num1;
-					}
-				} else if (num2 < 0) {
-					for(int i = 0; i > (num2 + 1); i--) {
-						result *= num1;
-					}
-					result = 1/result;
-				} else {
-					result = 1;
-				}
+			case '^':
+                result = (float) Math.pow(Float.parseFloat(arrOf[0]), Float.parseFloat(arrOf[1]));
+                System.out.println("Ответ: " + result);
+                break;
+			case '/':
+                result = Float.parseFloat(arrOf[0]) / Float.parseFloat(arrOf[1]);
+                System.out.println("Ответ: " + result);
 				break;
-			case "/":
-				result = num1 / num2;
-		} 
-		return result;
+            case '0':
+                System.out.println("Вы ввели некорректное выражение");
+		}
 	}
 }
--- src/com/startjava/lesson_2/Calculator/CalculatorTest.java
+++ src/com/startjava/lesson_2/Calculator/CalculatorTest.java
@@ -7,30 +7,12 @@ public class CalculatorTest {
 		Calculator calcOne = new Calculator();
 		Scanner in = new Scanner(System.in);
 		String answer;
+		String str;
 		do {
-			System.out.print("Введите первое число: ");
-			float firstNumber = in.nextFloat();
-			calcOne.setNum1(firstNumber);
-			//System.out.print("Вы ввели число: " + calcOne.getNum1());
-			System.out.print("Введите второе число: ");
-			float secondNumber = in.nextFloat();
-			calcOne.setNum2(secondNumber);
-			//System.out.print("Вы ввели число: " + calcOne.getNum2());
-			System.out.print("Введите операцию: ");
-			String addOperation = in.next();
-			calcOne.setOperation(addOperation);
-			String a = calcOne.getOperation();
-			while(!a.equals("+") && !a.equals("-") && !a.equals("%") && !a.equals("*") && !a.equals("^") && !a.equals("/")) {
-				if (a.equals("End")) {
-					System.out.print("Работа программы закончена");
-					return;
-				} else {
-					System.out.print("Введите корректную операцию (End для выхода): ");
-					calcOne.setOperation(in.next());
-					a = calcOne.getOperation();
-				}
-			} 
-			System.out.println("Ответ: " + calcOne.calculate());
+			System.out.println("Введите выражение: ");
+			str = in.next();
+			calcOne.takeSymbols(str);
+			calcOne.calculate();
 			do {
 				System.out.print("Хотите продолжить?(yes/no): ");
 				answer = in.next();
